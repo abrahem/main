f6ad2e3176cc779d30d7b5a53a4d9ae9e5a7c673
abrahem <abrheem16@gmail.com>
2020-04-19 19:25:26 +0300

    Update

--- src/boot/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-const axiosInstance = axios.create({
-  baseURL: 'https://snoanime.com/ns/api/new'
-})
-export default ({ Vue }) => {
-  Vue.prototype.$axios = axios
-}
-export { axiosInstance }
